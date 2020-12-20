fb558d448a31f388908f3514d9af2409e636d198
patricknavarre <patrick.navarre@codeimmersives.com>
2020-12-20 11:50:24 -0500

    styling-adjusted

--- main.js
+++ main.js
@@ -1,72 +1,40 @@
-// Query in Album Section
+// save the url and caption as properties in an object
+const albumObj = {url: "urlGoesHere", caption: "captionGoesHere..."}
+// save that object in an array
+const albumArr = [albumObj]
+// save that array as JSON in localStorage
+localStorage[albumObj] = JSON.stringify(albumObj);
+// add a global array that you can .push to
+
+
+
 const imageItem = (url, caption) => {
-const album = document.querySelector(".album");
+  const album = document.querySelector(".album");
+  const imageBox = document.createElement("div");
+  imageBox.classList = "image-box";
+  const newImage = document.createElement("img");
+  newImage.src = url;
+  const newPtag = document.createElement("p");
+  newPtag.innerText = caption;
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete";
+  imageBox.appendChild(newImage);
+  imageBox.appendChild(newPtag);
+  imageBox.appendChild(deleteButton);
+  deleteButton.addEventListener("click", () => {
+    imageBox.remove();
+  });
 
-  // Create a new element which is a div. Give that div a class of "image-box"
-
-const imageBox = document.createElement("div");
-imageBox.classList = "image-box";
-
-  // Create a new element for img.  Assign that image the url of the image.
-
-const newImage = document.createElement("img");
-newImage.src = url;
-
-  // Create a new element which is a <p> and then change the text of the p to the text of the user input.
-
-const newPtag = document.createElement("p");
-newPtag.innerText = caption;
-
-// creating another button for delete
-
-const newButton = document.createElement("button");
-newButton.innerText = 'delete';
-
-  // append the image to the <div> that was created.
-
-imageBox.appendChild(newImage);
-
-  // append the <p> tag to the <img> box div
-
-imageBox.appendChild(newPtag);
-
-// append the button
-
-imageBox.appendChild(newButton);
-
-newButton.addEventListener('click', () => {
-    imageBox.remove()
-});
-
-
-  // append the <div> box to the album <section>
-
-album.appendChild(imageBox);
+  album.appendChild(imageBox);
 };
-// Query in the button. addEventListener to the button. Write function for the addEventListener.
 
 const submitButton = document.querySelector(".submit-button");
 const deleteButton = document.querySelector(".delete-button");
 
 const submitButtonClicked = () => {
-const imageInput = document.querySelector("#image-input").value;
-const captionInput = document.querySelector("#caption-input").value;
-imageItem(imageInput, captionInput);
+  const imageInput = document.querySelector("#image-input").value;
+  const captionInput = document.querySelector("#caption-input").value;
+  imageItem(imageInput, captionInput);
 };
 
-// const deleteButtonClicked = (event) => {
-//     let buttonClicked = event.target;
-//     buttonClicked.classList.removeChild('image-box');
-// }
-
-// const removeImage = (item) => {
-//     item.target.remove()
-// }
-
-
 submitButton.addEventListener("click", submitButtonClicked);
-
-
-// Once button is clicked, it queries in the <input> with the URL. It will also query in the caption.
-
-// call the function. Pass in the URL and Caption from the query.
